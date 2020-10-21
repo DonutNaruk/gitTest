e3f1eeba1e813d5ae955dadded642077e9e29b66
Donut <nuttida.donut@mail.kmutt.ac.th>
2020-10-21 19:50:20 +0700

    BackToDonut

--- src/gittestagain/GitTestAgain.java
+++ src/gittestagain/GitTestAgain.java
@@ -15,22 +15,18 @@ public class GitTestAgain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         sayHello();
         sayGoodBye();
-        sayHi();
+
     }
     public static void sayHello(){
         System.out.println("Hello");
     }
     public static void sayGoodBye(){
         System.out.println("Good bye!");
-=======
-        // TODO code application logic here
->>>>>>> parent of 9b3e7b4 (Donut)
+
     }
-    public static void sayHi(){
-        System.out.println("Hi!");
-    }
+
     
 }
